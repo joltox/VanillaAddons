d7f8449c2ecdcf9136acadff899a4b3f7ccfc308
xzxKamixzx <mariomueller92@outlook.de>
2017-10-19 11:14:30 +0200

    Clean
    Clean

--- AuctionLink/AuctionLink/AuctionLink-enUS.lua
+++
@@ -1,9 +0,0 @@
-local L = AceLibrary("AceLocale-2.2"):new("AuctionLink")
-L:RegisterTranslations("enUS", function()
-	return {
-		["autosearch"] = true,
-		["Automatically search after clicking an item"] = true,
-		["/alink"] = true,
-		["/auctionlink"] = true
-	}
-end)
